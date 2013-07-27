dcc0f3c1f3f1a83ec90babd52c4e22bd6f2ba453
Jan Nemecek <Nemecek.Jan@seznam.cz>
2013-07-28 01:50:20 +0200

    Added TextureManager class. It loads sf::Image and creates sf::Texture from it. Still working on Image "widget".
    Signed-off-by: Jan Nemecek <Nemecek.Jan@seznam.cz>

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 
 #include <SFML/Graphics.hpp>
 
+#include "Utils/Utils.hpp"
 #include "Logger.hpp"
 
 namespace WhoreMasterRenewal
@@ -15,9 +16,15 @@ struct ScreenPosition
         Y( y )
     {}
     
+    ScreenPosition( const sf::Vector2f& pos ) :
+        X( static_cast<int>( pos.x ) ),
+        Y( static_cast<int>( pos.y ) )
+    {}
+    
     int X = 0;
     int Y = 0;
 };
+typedef ScreenPosition TexturePosition;
 
 struct ScreenSize
 {
@@ -29,6 +36,7 @@ struct ScreenSize
     int Width = 0;
     int Height = 0;
 };
+typedef ScreenSize TextureSize;
 
 struct ScreenRectangle
 {
@@ -40,6 +48,7 @@ struct ScreenRectangle
     ScreenPosition Position = {0,0};
     ScreenSize Size = {0,0};
 };
+typedef ScreenRectangle TextureRectangle;
 
 class Drawable
 {
@@ -47,64 +56,228 @@ public:
     virtual ~Drawable()
     {}
     
-    virtual void Draw() = 0;
+    virtual void Draw( std::weak_ptr<sf::RenderTarget> renderTarget ) = 0;
     
-    void SetPosition( ScreenPosition position = {0,0} );
-    ScreenPosition GetPosition();
-    void SetSize( ScreenSize size = {0,0} );
-    ScreenSize GetSize();
-    void SetRectangle( ScreenRectangle = { {0,0}, {0,0} } );
-    ScreenRectangle GetRectangle();
-    bool IsVisible();
-    void SetVisible( bool visible = false );
+    void SetPosition( const ScreenPosition& position = {0,0} )
+    {
+        m_Rectangle.Position = position;
+    }
+    
+    const ScreenPosition& GetPosition() const
+    {
+        return ScreenPosition( m_Rectangle.Position );
+    }
+    
+    void SetSize( const ScreenSize& size = {0,0} )
+    {
+        m_Rectangle.Size = size;
+    }
+    
+    const ScreenSize& GetSize() const
+    {
+        return m_Rectangle.Size;
+    }
+    
+    void SetRectangle( const ScreenRectangle& rect = { {0,0}, {0,0} } )
+    {
+        m_Rectangle = rect;
+    }
+    
+    const ScreenRectangle& GetRectangle() const
+    {
+        return m_Rectangle;
+    }
+    
+    void SetVisible( bool visible = false )
+    {
+        m_Visible = visible;
+    }
+    
+    bool IsVisible() const
+    {
+        return m_Visible;
+    }
     
 private:
     ScreenRectangle m_Rectangle = { {0,0}, {0,0} };
     bool m_Visible = true;
 };
 
-class InterfaceObject
+class Object
 {
 public:
     int m_ID;
 };
 
-class InterfaceButton : Drawable
+class Button : Drawable
 {
     
 };
 
-class InterfaceCheckBox : Drawable
+class CheckBox : Drawable
 {
     
 };
 
-class InterfaceEditBox : Drawable
+class EditBox : Drawable
 {
 };
 
-class InterfaceImageItem : Drawable
+class TextureManager
+{
+public:
+    TextureManager()
+    {
+    }
+    
+    ~TextureManager()
+    {
+    }
+    
+    TextureManager( const TextureManager& ) = delete;
+    TextureManager& operator = ( const TextureManager& ) = delete;
+    
+    std::shared_ptr<sf::Image> LoadImage( std::string path )
+    {
+        // Check if image is loaded first
+        if( m_Images.find( path ) != m_Images.end() )
+        {
+            return m_Images[path];
+        }
+        
+        // Create new image entry, result is std::pair<iter,bool>
+        auto result = m_Images.emplace( path, std::make_shared<sf::Image>() );
+        if( !result.second ) // false if emplace failed
+        {
+            Logger() << Utils::formatString(
+                            "TextureManager Error: cannot save {%} image to repository.\n", path );
+        }
+        auto image = (*result.first).second;
+        
+        if( !image->loadFromFile( path ) )
+        {
+            // TODO: Generate image not found here
+            Logger() << Utils::formatString( "Loading of image \"{%}\" failed.\n", path );
+        }
+        else
+        {
+            Logger() << Utils::formatString( "TextureManager: \"{%}\" loaded.", path ) << "\n";
+        }
+        
+        return image;
+    }
+    
+    std::shared_ptr<sf::Texture> GetTexture( std::string path )
+    {
+        if( m_Textures.find( path ) != m_Textures.end() )
+        {
+            return m_Textures[path];
+        }
+        
+        // Create new texture entry, result is std::pair<iter,bool>
+        auto result = m_Textures.emplace( path, std::make_shared<sf::Texture>() );
+        if( !result.second ) // false if emplace failed
+        {
+            Logger() << Utils::formatString(
+                            "TextureManager Error: cannot save {%} texture to repository.\n", path );
+        }
+        auto texture = (*result.first).second; // iterator - std::pair<string, shared_ptr<Texture>>
+        
+        texture->loadFromImage( *(LoadImage(path)) );
+        
+        return texture;
+    }
+    
+    std::map<std::string, std::shared_ptr<sf::Image>> m_Images = {};
+    std::map<std::string, std::shared_ptr<sf::Texture>> m_Textures = {};
+};
+
+class Image : public Drawable
+{
+public:
+    Image( const std::shared_ptr<TextureManager>& manager ) :
+        m_TextureManager( manager )
+    {
+    }
+    
+    virtual ~Image() override
+    {
+    }
+    
+    virtual void Draw( std::weak_ptr<sf::RenderTarget> renderTarget ) override
+    {
+        if( !m_Texture )
+            return;
+        
+        if( auto rt = renderTarget.lock() )
+        {
+            rt->draw( m_Sprite );
+        }
+    }
+    
+    void LoadImage( std::string path )
+    {
+        m_Texture = m_TextureManager->GetTexture( path );
+        m_Sprite.setTexture( *m_Texture );
+        
+        Drawable::SetPosition( m_Sprite.getPosition() );
+    }
+    
+    void SetTextureRectangle( TextureRectangle rect )
+    {
+        
+    }
+    
+    std::shared_ptr<TextureManager> m_TextureManager;
+    std::shared_ptr<sf::Texture> m_Texture = nullptr;
+    sf::Sprite m_Sprite = {};
+};
+
+class Label : Drawable
 {
 };
 
-class InterfaceWindow : Drawable
+class ListBox : Drawable
 {
 };
 
-class InterfaceListBox : Drawable
+class ScrollBar : Drawable
 {
 };
 
-class InterfaceScrollBar : Drawable
+class Slider : Drawable
 {
 };
 
-class InterfaceSlider : Drawable
+class TextItem : Drawable
 {
 };
 
-class InterfaceTextItem : Drawable
+class Window : Drawable
 {
+public:
+    Window()
+    {
+        Logger() << "Window created...\n";
+    }
+    
+    virtual ~Window() override
+    {
+        Logger() << "Window destroyed...\n";
+    }
+    
+    virtual void Draw( std::weak_ptr<sf::RenderTarget> renderTarget ) override
+    {
+        if( auto rt = renderTarget.lock() )
+        {
+            for( auto child : m_Children )
+            {
+                child->Draw( rt );
+            }
+        }
+    }
+    
+    std::vector<std::shared_ptr<Drawable>> m_Children = {};
 };
 
 
@@ -117,7 +290,8 @@ public:
     {}
     
     WindowManager( std::shared_ptr<sf::RenderWindow> renderWindow )
-        : m_RenderWindow( renderWindow )
+        : m_RenderWindow( renderWindow ),
+          m_TextureManager( std::make_shared<TextureManager>() )
     {
         Logger() << "WindowManager: created...\n";
     }
@@ -126,6 +300,8 @@ public:
     {
         if( m_RenderWindow )
             m_RenderWindow = nullptr;
+        
+        Logger() << "WindowManager: finished...\n";
     }
     
     void Initialise()
@@ -141,11 +317,44 @@ public:
     {
         if( !m_RenderWindow->isOpen() )
             this->Initialise();
+        m_RenderWindow->setFramerateLimit( 60 );
+        
+        std::shared_ptr<Window> window = std::make_shared<Window>();
+        
+        
+        auto image1 = std::make_shared<Image>( m_TextureManager);
+        image1->LoadImage( "Resources/Images/Sizes.jpeg" );
+        
+        auto image2 = std::make_shared<Image>(m_TextureManager);
+        image2->LoadImage( "Resources/Images/200x100.png" );
+        
+        auto image3 = std::make_shared<Image>(m_TextureManager);
+        image3->LoadImage( "Resources/Images/300x50.bmp" );
+        
+        auto image4 = std::make_shared<Image>(m_TextureManager);
+        image4->LoadImage( "Resources/Images/15x15.gif" );
+        
+        auto image5 = std::make_shared<Image>(m_TextureManager);
+        image5->LoadImage( "Resources/Images/multiple.tga" );
+        
+        auto image6 = std::make_shared<Image>(m_TextureManager);
+        image6->LoadImage( "Resources/Images/Button.png" );
+        image6->m_Sprite.setColor( {255,0,255} );
+        
+        window->m_Children.emplace_back( image1 );
+        window->m_Children.emplace_back( image2 );
+        window->m_Children.emplace_back( image3 );
+        window->m_Children.emplace_back( image4 );
+        window->m_Children.emplace_back( image5 );
+        window->m_Children.emplace_back( image6 );
         
         while( m_RenderWindow->isOpen() )
         {
             
             m_RenderWindow->clear();
+            
+            window->Draw( m_RenderWindow );
+            
             m_RenderWindow->display();
             
             sf::Event event;
@@ -154,6 +363,13 @@ public:
                 
                 switch( event.type )
                 {
+                case sf::Event::Resized:
+                    {
+                        sf::View visibleArea( sf::FloatRect( 0.0, 0.0, event.size.width, event.size.height ) );
+                        m_RenderWindow->setView( visibleArea );
+                    }
+                    break;
+                    
                 case sf::Event::Closed:
                     // TODO: Closing event
                     m_RenderWindow->close();
@@ -180,15 +396,12 @@ public:
                 case sf::Event::MouseLeft:
                 case sf::Event::MouseMoved:
                 case sf::Event::MouseWheelMoved:
-                case sf::Event::Resized:
                 case sf::Event::TextEntered:
                     break;
                     
                 default: // We ignore the rest of events
                     break;
                 }
-                2013-07-12-21.55
-                2013-07-12-21.55.32
             }
             
         }
@@ -202,7 +415,8 @@ public:
     }
     
 private:
-    std::shared_ptr<sf::RenderWindow> m_RenderWindow = nullptr;
+    std::shared_ptr<sf::RenderWindow> m_RenderWindow;
+    std::shared_ptr<TextureManager> m_TextureManager;
 };
 
 
